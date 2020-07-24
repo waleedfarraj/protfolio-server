a0f5756c4d9a1c43db80b6e8d403808579600d64
waleedfarraj <waleedfarraj@LTUC-STDL066.localdomain>
2020-07-24 20:30:59 +0300

    can't get html

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 "use strict"
 // declaring a varialble to use express library
 const express = require("express");
-const { request, response } = require("express");
 // require calling the library 
 
 // intialize the server 
@@ -13,11 +12,12 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT,() => { 
     console.log('I am listening to port' , PORT);
 });
-server.use(express.static("./puplic"));
-
 server.get("/test",(request,response)=>{
     response.send("you are awsome");
 })
+server.use(express.static("/puplic "));
+
+
 // http://localhost:3000/test
 
 server.get('/data',(request,response)=>{
